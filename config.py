0987073871909f6d228c659271b13b1287570caf
om705 <61368509+om705@users.noreply.github.com>
2024-02-07 20:34:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-API_ID = API_ID =  25703106
+API_ID = API_ID =  6960732076
 
-API_HASH = os.environ.get("API_HASH", "75cb4ce927cd3da265bbf86942c371f9")
+API_HASH = os.environ.get("API_HASH", "d7abcec5c967414fadb1d438fa05ebea")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6896439329:AAFSJV97z7zzot3wmJUVi7uwLO9p2UW7lSU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6960732076:AAHuNQn7fB7RGfEW-BZM8t6SB8AaJGzFqBQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER",5959755604))
+OWNER = int(os.environ.get("OWNER",1403488629))
 
-LOG = -1002144796510 
+LOG = -1001998453704 
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5760012562 5044896938 6116624490 6230767081 5496342413 6347607291 6351476810").split()):
+    for x in (os.environ.get("ADMINS", "1403488629 5044896938 6116624490 6230767081 5496342413 6347607291 6351476810").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
